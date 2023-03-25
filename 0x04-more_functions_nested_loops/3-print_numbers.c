156fd1c0711d384e67d39275eb6bbb1f92e44e93
kalthoum11 <kalthoumibrahimi@gmail.com>
2023-03-24 18:31:00 -0700

    0x04..

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0-9
- * return: void
- */
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
 
 void print_numbers(void)
 {
@@ -11,23 +12,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c);
+	_putchar(c);
 	}
 	_putchar('\n');
-}
-
-
-3-main.c
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-    print_numbers();
-    return (0);
 }
